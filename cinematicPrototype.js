8697663774188f34b1bbe053e55a86db5b830ae8
connor-lynchh <cowlynch@ucsc.edu>
2023-06-11 23:51:15 -0700

    removed physics for now

--- cinematicPrototype.js
+++ cinematicPrototype.js
@@ -129,41 +129,6 @@ class Load extends Phaser.Scene{
 
     create() {
 
-        gameState.active=true;
-
-        gameState.cursors = this.input.keyboard.createCursorKeys();
-
-        this.player = this.physics.add.sprite(100, 450, 'dude');
-
-        this.physics.world.setBounds(0, 0, gameState.width, gameState.height + this.player.height);
-
-        
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-
-
         var queen = this.add.image(1000,500,'queen')
         queen.setScale(10);
 
@@ -256,33 +221,9 @@ class Load extends Phaser.Scene{
 
 
 
-
         this.input.on('pointerdown', () => this.scene.start('intro'));
     }
-    update ()
-    {
-        const { left, right, up } = this.cursors;
 
-        if (left.isDown)
-        {
-            this.player.setVelocityX(-160);
-
-            this.player.anims.play('left', true);
-        }
-        else if (right.isDown)
-        {
-            this.player.setVelocityX(160);
-
-            this.player.anims.play('right', true);
-        }
-        else
-        {
-            this.player.setVelocityX(0);
-
-            this.player.anims.play('turn');
-        }
-    
-}
 }
 const game = new Phaser.Game({
     scale: {
@@ -290,7 +231,7 @@ const game = new Phaser.Game({
         pixelArt: true,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1920,
-        height: 1080
+        height: 1080,
     },
     scene: [Intro,Load],
     title: "Cinematic",
